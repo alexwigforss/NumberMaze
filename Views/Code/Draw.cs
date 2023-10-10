4cc4882ae055879611e7c9d8ef9a947841916451
Alex Wigforss <alex.wigforss@gmail.com>
2023-10-10 17:02:49 +0200

    kommenterat ut innehållet i Draw()

--- Views/Code/Draw.cs
+++ Views/Code/Draw.cs
@@ -4,9 +4,9 @@ public class TestDrawable : IDrawable
 {
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
-        canvas.StrokeColor = Colors.Aqua;
-        canvas.StrokeSize = 1;
-        canvas.DrawCircle(150, 100, 50);
-        canvas.DrawRoundedRectangle(200, 200, 100, 100, 25);
+        //canvas.StrokeColor = Colors.Aqua;
+        //canvas.StrokeSize = 1;
+        //canvas.DrawCircle(150, 100, 50);
+        //canvas.DrawRoundedRectangle(200, 200, 100, 100, 25);
     }
 }
